a5d2a009dfd9a84852fc3d8a55b8148ad619ae05
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-05 19:34:39 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+  }
+
+
+  const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+const writeCards = (array = ["Aki", "Ollie", "Guadalupe"]) => {
+    const messageArray = [];
+    for(const a of array) {
+        messageArray.push(`Thank you, ${a}, for the wonderful surprise gift!`);
+    }
+    return messageArray;
+}
+
+const countDown = (num = 10) => {
+    for(let number = num; number >= 0; number--){
+        console.log(number)
+    }
+}
